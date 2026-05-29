9db9447ec07ddbaa854d18aa5e729eca471fa992
JoseAndres08 <josenadrescovenas@gmail.com>
2026-05-28 22:47:19 -0500

    fix: Age nullable para evitar crash en /api/Pets

--- DTOs/CreatePetDto.cs
+++ DTOs/CreatePetDto.cs
@@ -5,7 +5,7 @@
         public string Name { get; set; } = "";
         public string Species { get; set; } = "";
         public string Breed { get; set; } = "";
-        public int Age { get; set; }
+        public int? Age { get; set; }
         public string Sex { get; set; } = "";
         public string LastName { get; set; } = "";
     }
--- Modelos/Pet.cs
+++ Modelos/Pet.cs
@@ -15,7 +15,7 @@ namespace BackendLimpio.Models
 
         public string Breed { get; set; } = "";
 
-        public int Age { get; set; }
+        public int? Age { get; set; }
 
         public string Sex { get; set; } = "";
 
